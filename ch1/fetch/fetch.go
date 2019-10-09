b5475e54e16c5e30d609705b5e9221a1ec5f8cbb
Manoj Guglani <manoj@orkus.com>
2019-10-09 10:10:50 -0700

    added support for url prefix

--- ch1/fetch/fetch.go
+++ ch1/fetch/fetch.go
@@ -6,10 +6,15 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
+	const prefix = "http://"
 	for _, url := range os.Args[1:] {
+		if strings.HasPrefix(url, prefix) == false {
+			url = prefix + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
@@ -21,6 +26,6 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%d bytes read\n", b)
+		fmt.Printf("\n%d bytes read\n", b)
 	}
 }
