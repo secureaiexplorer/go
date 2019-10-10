b349c988f3fdc8ab9d8a1ebeab71b3000a038d83
Manoj Guglani <manoj@orkus.com>
2019-10-10 09:21:29 -0700

    fetching urls concurrently

---
+++ ch1/fetchall/fetchall.go
@@ -0,0 +1,53 @@
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+// Exercise 1.10: Find a web site that produces a large amount of data. Investigate caching by running fetchall
+// twice in succession to see whether the reported time changes much. Do you get the same content
+// each time? Modify fetchall to print its output to a file so it can be examined.
+// Exercise 1.11: Try fetchall with longer argument lists, such as samples from the top million web sites available
+// at alexa.com. How does the program behave if a web site just doesn’t respond? (Section 8.9 describes
+// mechanisms for coping in such cases.)
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	const prefix = "http://"
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		if strings.HasPrefix(url, prefix) == false {
+			url = prefix + url
+		}
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
